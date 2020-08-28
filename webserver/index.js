58b11047b71ce7532460313a8a1c24a0ba661e3c
papabyte <bcreze@protonmail.com>
2020-08-28 12:30:32 +0200

    local time on dashboard

--- webserver/index.js
+++ webserver/index.js
@@ -3,6 +3,8 @@ const frontApp = require('../front_app.js');
 const express = require('express')
 const path = require('path');
 
+
+
 if (conf.isHighAvailabilityNode){ // in high availability mode, an external MySQL DB is to be used
 	var appDB = require('./modules/external_db.js');
 } else {
@@ -26,15 +28,14 @@ function start(){
 		const channels = await appDB.query("SELECT unconfirmed_status,status,amount_spent_by_me,amount_spent_by_peer,asset,peer_address,channels.aa_address,\n\
 		amount_deposited_by_me,my_payments_count,peer_payments_count,close_timestamp,\n\
 		(SELECT amount FROM payments_sent WHERE payments_sent.aa_address=channels.aa_address ORDER BY ID DESC LIMIT 1) AS last_payment_sent_amount,\n\
-		(SELECT date FROM payments_sent WHERE payments_sent.aa_address=channels.aa_address ORDER BY ID DESC LIMIT 1) AS last_payment_sent_date,\n\
+		(SELECT strftime('%s',date) FROM payments_sent WHERE payments_sent.aa_address=channels.aa_address ORDER BY ID DESC LIMIT 1) AS last_payment_sent_date,\n\
 		last_response_unit,last_unconfirmed_status_unit,\n\
 		IFNULL((SELECT SUM(amount) FROM my_deposits WHERE my_deposits.aa_address=channels.aa_address AND is_confirmed_by_aa=0),0) AS my_pending_deposit\n\
 		FROM channels LEFT JOIN unconfirmed_units_from_peer ON unconfirmed_units_from_peer.aa_address=unconfirmed_units_from_peer.aa_address\n\
 		");
-
 		res.render('index.ejs', {
 			channels,
-			conf
+			conf,
 		});
 	});
 
